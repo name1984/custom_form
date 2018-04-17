4c032ff8883428913f75f95f49e54c580322256e
Néstor Mendoza <nestor.mendoza.dev@gmail.com>
2018-04-17 10:34:10 -0500

    CORRECION
    CORRECCION DE ERROR DE TIPEO EN LA LINEA 9

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ class ExtraInfoForm(ModelForm):
     The field on this form are derived from the ExtraInfo model in models.py
     """
     def __init__(self, *args, **kwargs):
-        super(ExtraInfoForm, selft).__init__(*args, **kwargs)
+        super(ExtraInfoForm, self).__init__(*args, **kwargs)
         self.fields['cedula'].error_messages ={
             "required" : "ES NECESARIO INGRESAR SU CEDULA DE CIUDADANIA",
             "invalid" : "INGRESE UNA CEDULA VALIDA",
